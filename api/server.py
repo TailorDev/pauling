00466cec904da3246a964c4f42204d69ea19017b
William Durand <will+git@drnd.me>
2017-08-07 14:57:55 +0200

    [api] Add Sass, Autoprefixer, Flask template + Makefile

--- api/server.py
+++ api/server.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask import render_template
 
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/hello/<name>')
+def hello_world(name=None):
+    return render_template('hello.html', name=name)

f98fa15e96bc3bad012aa8047cc30f00996e8697
William Durand <will+git@drnd.me>
2017-08-09 13:04:41 +0200

    [api] force QR code PNG size

--- api/qr/png.py
+++ api/qr/png.py
@@ -12,6 +12,7 @@ def make_png(poster):
     qr = qrcode.QRCode(border=6)
     qr.add_data(poster.public_url(absolute=True))
     code = qr.make_image()
+    code = code.resize((490, 490), resample=Image.BICUBIC)
     img = Image.open('{}/qr-template.png'.format(img_dir))
     img.paste(code, (5, 105))
     img.save(buf, format='png')

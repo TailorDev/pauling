7db200b6d476c52ec73a290a4e28cf2a31c29f26
William Durand <will+git@drnd.me>
2017-08-09 11:35:44 +0200

    [api] replace png customization by template

--- api/qr/png.py
+++ api/qr/png.py
@@ -1,21 +1,19 @@
+import os
 from io import BytesIO
 
 import qrcode
 import qrcode.image.svg
 from PIL import Image, ImageColor, ImageDraw, ImageFont
 
+img_dir = os.path.dirname(os.path.abspath(__file__))
 
 def make_png(poster):
     buf = BytesIO()
     qr = qrcode.QRCode(border=6)
     qr.add_data(poster.public_url(absolute=True))
-    img = qr.make_image()
-    img_draw = ImageDraw.Draw(img)
-    font_title = ImageFont.truetype('Lato-Medium.ttf', 24)
-    font = ImageFont.truetype('Lato-Medium.ttf', 18)
-    img_draw.text((45, 3), 'View this poster with the Pauling app*', font=font_title)
-    img_draw.text((100, 440), '*install the iPhone/Android app via', font=font)
-    img_draw.text((140, 460), 'pauling.lelab.tailordev.fr', font=font)
-    img.save(buf)
+    code = qr.make_image()
+    img = Image.open('{}/qr-template.png'.format(img_dir))
+    img.paste(code, (5, 105))
+    img.save(buf, format='png')
     buf.seek(0)
     return buf

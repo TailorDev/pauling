b9ffa19de973825c4b46e24966d0997abb77ad5f
Julien Maupetit <julien@tailordev.fr>
2017-08-16 12:37:34 +0200

    Define/use props with better fixtures for the PosterCard component

--- mobile/src/PosterCard/presenter.js
+++ mobile/src/PosterCard/presenter.js
@@ -13,33 +13,35 @@ import {
   Body,
   Right,
 } from 'native-base';
+import Moment from 'moment';
 import styles from './styles';
 
+type Props = {
+  title: String,
+  authors: String,
+  thumbnailUrl: String,
+  savedAt: Moment,
+};
 
-class PosterCard extends Component {
-  render() {
-    return (
-      <Card style={styles.PosterCard}>
-        <CardItem cardBody>
-          <Image
-            source={{ uri: 'https://s3-eu-west-1.amazonaws.com/pfigshare-u-previews/1710259/thumb.png' }}
-            style={styles.PosterThumbnail}
-          />
-          <View style={styles.PosterInfos}>
-            <Text style={styles.PosterTitle}>
-              Imperfect centered sites - a new mode of miRNA binding
-            </Text>
-            <Text style={styles.PosterAuthors}>
-              Nicole Cloonan
-            </Text>
-            <Text style={styles.PosterSavedAt}>
-              Saved: 11h ago
-            </Text>
-          </View>
-        </CardItem>
-      </Card>
-    );
-  }
-}
+const PosterCard = (props: Props) =>
+  <Card style={styles.PosterCard}>
+    <CardItem cardBody>
+      <Image
+        source={{ uri: props.thumbnailUrl }}
+        style={styles.PosterThumbnail}
+      />
+      <View style={styles.PosterInfos}>
+        <Text style={styles.PosterTitle}>
+          {props.title}
+        </Text>
+        <Text style={styles.PosterAuthors}>
+          {props.authors}
+        </Text>
+        <Text style={styles.PosterSavedAt}>
+          Saved: {props.savedAt.format('YYYY/MM/DD, HH:mm a')}
+        </Text>
+      </View>
+    </CardItem>
+  </Card>;
 
 export default PosterCard;
--- mobile/src/PosterCardList/presenter.js
+++ mobile/src/PosterCardList/presenter.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react';
 import { FlatList } from 'react-native';
+import Moment from 'moment';
 
 import PosterCard from '../PosterCard';
 
-export default class PosterCardList extends Component {
-  render() {
-    return (
-      <FlatList
-          data={[
-            {key: 'Devin'},
-            {key: 'Jackson'},
-            {key: 'James'},
-            {key: 'Joel'},
-            {key: 'John'},
-            {key: 'Jillian'},
-            {key: 'Jimmy'},
-            {key: 'Julie'},
-          ]}
-          renderItem={({item}) => <PosterCard />}
-      />
-    );
-  }
-}
+const posters = [
+  {
+    key: 'uuid-01',
+    title: 'Imperfect centered sites - a new mode of miRNA binding',
+    thumbnailUrl: 'https://s3-eu-west-1.amazonaws.com/pfigshare-u-previews/1710259/thumb.png',
+    authors: 'Nicole Cloonan',
+    savedAt: Moment()
+  },
+  {
+    key: 'uuid-02',
+    title: 'The Value Proposition of Libraries in Research Information Management',
+    thumbnailUrl: 'https://s3-eu-west-1.amazonaws.com/pfigshare-u-previews/9011278/thumb.png',
+    authors: 'Rebecca Bryant, Holly Mercer',
+    savedAt: Moment()
+  },
+];
+
+const PosterCardList = () =>
+  <FlatList
+    data={posters}
+    renderItem={({item}) => <PosterCard {...item} />}
+  />;
+
+export default PosterCardList;

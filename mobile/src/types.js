51c800fd98a4496e625ed9ee696957fb8ca8d310
Julien Maupetit <julien@tailordev.fr>
2017-08-17 11:18:30 +0200

    Add Navigation type

--- mobile/src/types.js
+++ mobile/src/types.js
@@ -2,6 +2,12 @@
 import Moment from 'moment';
 
 
+export type Navigation = {
+  navigate: Function,
+  addListener: Function,
+  removeListener: Function,
+};
+
 export type Poster = {
   key: string,
   title: string,

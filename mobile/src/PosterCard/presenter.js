fef3ddc82a7eee3e0cf5f176c05fc31048086fb0
Julien Maupetit <julien@tailordev.fr>
2017-08-17 11:10:46 +0200

    Make PosterCard touchable

--- mobile/src/PosterCard/presenter.js
+++ mobile/src/PosterCard/presenter.js
@@ -1,35 +1,32 @@
 /* @flow */
 import React from 'react';
 
-import { Image, View } from 'react-native';
+import { Image, TouchableOpacity, View } from 'react-native';
 import { Text } from 'native-base';
-import Moment from 'moment';
+
 import styles from './styles';
+import type { Poster } from '../types';
 
-type Props = {
-  title: string,
-  authors: string,
-  thumbnailUrl: string,
-  savedAt: Moment,
-};
 
-const PosterCard = (props: Props) =>
-  <View style={styles.PosterCard}>
-    <Image
-      source={{ uri: props.thumbnailUrl }}
-      style={styles.PosterThumbnail}
-    />
-    <View style={styles.PosterInfos}>
-      <Text style={styles.PosterTitle}>
-        {props.title}
-      </Text>
-      <Text style={styles.PosterAuthors}>
-        {props.authors}
-      </Text>
-      <Text style={styles.PosterSavedAt}>
-        Saved: {props.savedAt.format('YYYY/MM/DD, HH:mm a')}
-      </Text>
+const PosterCard = (props: Poster) =>
+  <TouchableOpacity>
+    <View style={styles.PosterCard}>
+      <Image
+        source={{ uri: props.thumbnailUrl }}
+        style={styles.PosterThumbnail}
+      />
+      <View style={styles.PosterInfos}>
+        <Text style={styles.PosterTitle}>
+          {props.title}
+        </Text>
+        <Text style={styles.PosterAuthors}>
+          {props.authors}
+        </Text>
+        <Text style={styles.PosterSavedAt}>
+          Saved: {props.savedAt.format('YYYY/MM/DD, HH:mm a')}
+        </Text>
+      </View>
     </View>
-  </View>;
+  </TouchableOpacity>;
 
 export default PosterCard;

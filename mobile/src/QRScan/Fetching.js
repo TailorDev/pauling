73f6378dc782f9ff18c277e0c8edfab9f4ae668f
Julien Maupetit <julien@tailordev.fr>
2017-08-22 22:19:40 +0200

    Make Fetching a functional component

--- mobile/src/QRScan/Fetching.js
+++ mobile/src/QRScan/Fetching.js
@@ -1,37 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import { View } from 'react-native';
 import { Spinner, Text } from 'native-base';
 
 import { colors } from '../settings';
-import type { Poster } from '../types';
 import styles from './styles';
 
-type Props = {};
 
-
-class Fetching extends Component {
-
-  props: Props;
-
-  constructor(props: Props) {
-    super(props);
-
-    this.state = {
-      hasFetchedData: false,
-    };
-  };
-
-  render() {
-    return (
-      <View style={styles.Fetching}>
-        <Text style={styles.FetchingMessage}>
-          Collecting poster from Pauling database…
-        </Text>
-        <Spinner color={colors.textAltPrimaryColor} />
-      </View>
-    );
-  }
-}
+const Fetching = () => (
+  <View style={styles.Fetching}>
+    <Text style={styles.FetchingMessage}>
+      Collecting poster from Pauling database…
+    </Text>
+    <Spinner color={colors.textAltPrimaryColor} />
+  </View>
+);
 
 export default Fetching;

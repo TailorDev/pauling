ebe05290242f3c2b246191e7139932500f8de955
Julien Maupetit <julien@tailordev.fr>
2017-08-31 11:13:07 +0200

    Name app in reactotron configuration

--- mobile/src/ReactotronConfig.js
+++ mobile/src/ReactotronConfig.js
@@ -2,7 +2,7 @@ import Reactotron from 'reactotron-react-native';
 import { reactotronRedux } from 'reactotron-redux';
 
 Reactotron
-  .configure()
+  .configure({name: 'Pauling'})
   .useReactNative()
   .use(reactotronRedux())
   .connect();

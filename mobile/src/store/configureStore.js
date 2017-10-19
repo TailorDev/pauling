62ebce421490584aba5ba01d342ba8d8668dea5d
William Durand <will+git@drnd.me>
2017-10-19 15:31:41 +0200

    Remove Reactotron

--- mobile/src/store/configureStore.js
+++ mobile/src/store/configureStore.js
@@ -2,13 +2,11 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import { persistStore, autoRehydrate } from 'redux-persist';
 import { AsyncStorage } from 'react-native';
-import Reactotron from 'reactotron-react-native';
 import thunk from 'redux-thunk';
 
 import rootReducer from 'app/reducers';
 
 const middlewares = [thunk];
-const createPaulingStore = __DEV__ ? Reactotron.createStore : createStore;
 
 if (__DEV__) {
   const { logger } = require('redux-logger'); // eslint-disable-line
@@ -17,7 +15,7 @@ if (__DEV__) {
 }
 
 export default function configureStore(onComplete: Function) {
-  const store = createPaulingStore(
+  const store = createStore(
     rootReducer,
     compose(applyMiddleware(...middlewares), autoRehydrate())
   );
